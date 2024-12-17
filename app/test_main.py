63f95b9163c307e764a7b64bf985f762d0c0cabc
midetc <mikhailetc.artem@gmail.com>
2024-12-17 19:06:07 +0200

    add solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,44 @@
-# write your code here
+import datetime
+import pytest
+from unittest import mock
+from app.main import outdated_products
+
+
+class TestOutdatedProducts:
+    @pytest.mark.parametrize(
+        "input_products, expected_expired_products, mocked_today", [
+            (
+                    [
+                        {"name": "salmon",
+                         "expiration_date": datetime.date(2022, 2, 10),
+                         "price": 600},
+                        {"name": "chicken",
+                         "expiration_date": datetime.date(2022, 2, 5),
+                         "price": 120},
+                    ],
+                    ["salmon", "chicken"],
+                    datetime.date(2022, 2, 20),
+            ),
+            (
+                    [
+
+                        {"name": "chicken",
+                         "expiration_date": datetime.date(2022, 2, 5),
+                         "price": 120},
+                    ],
+                    [],
+                    datetime.date(2022, 2, 5),
+            ),
+        ]
+    )
+    @mock.patch("datetime.date")
+    def test_given_products_and_date_return_expired_products(
+            self,
+            mock_date,
+            input_products,
+            expected_expired_products,
+            mocked_today,
+    ):
+        mock_date.today.return_value = mocked_today
+        result = outdated_products(input_products)
+        assert result == expected_expired_products, f"Expected: {expected_expired_products}, Got: {result}"
